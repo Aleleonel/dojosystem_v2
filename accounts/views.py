4069aaf887fcb2e230a98562f29cb32069a5ec46
Alexandre <aleleonel@gmail.com>
2026-05-09 12:43:44 -0300

    Pagina de login/logout e redirecionamento

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
 
-# Create your views here.
+
+class CustomLoginView(LoginView):
+    template_name = 'registration/login.html'
+
+
+class CustomLogoutView(LogoutView):
+    pass
--- config/urls.py
+++ config/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('', include('dashboard.urls')),
+    path('accounts/', include('accounts.urls')),
 
 ]
 
