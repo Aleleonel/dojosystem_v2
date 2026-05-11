f8caab57bb10a33a3937a9ca4467ef1907c2da06
Alexandre <aleleonel@gmail.com>
2026-05-11 08:47:53 -0300

    dashbord com grafico real

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,10 +3,18 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 from django.utils.timezone import now
+from aulas.models import Aula
 
 from alunos.models import Aluno
 from aulas.models import Presenca
 from financeiro.models import Mensalidade
+
+from django.db.models import Sum
+from django.utils.timezone import now
+
+from django.db.models.functions import ExtractMonth
+
+from django.db.models import Count
 
 
 @login_required
@@ -48,6 +56,65 @@ def dashboard(request):
         academia=academia
     )
 
+    total_aulas = Aula.objects.filter(
+    academia=request.user.academia
+    ).count()
+
+    presencas_por_mes = (
+        Presenca.objects.filter(
+        aula__academia=request.user.academia
+    )
+    .annotate(
+       mes=ExtractMonth('data_hora_presenca')
+    )
+    .values('mes')
+    .annotate(total=Count('id'))
+    .order_by('mes')
+    )
+
+    meses = []
+    totais = []
+
+    nomes_meses = {
+        1: 'Jan',
+        2: 'Fev',
+        3: 'Mar',
+        4: 'Abr',
+        5: 'Mai',
+        6: 'Jun',
+        7: 'Jul',
+        8: 'Ago',
+        9: 'Set',
+        10: 'Out',
+        11: 'Nov',
+        12: 'Dez',
+    }
+
+    for item in presencas_por_mes:
+
+        meses.append(
+            nomes_meses[item['mes']]
+        )
+
+        totais.append(
+            item['total']
+        )
+
+    mes_atual = now().month
+    mensalidades_pagas = Mensalidade.objects.filter(
+        status='PAGO',
+        academia=request.user.academia
+    )
+
+    faturamento = mensalidades_pagas.aggregate(
+        total=Sum('valor')
+    )['total'] or 0
+
+    inadimplentes = Mensalidade.objects.filter(
+        status='PENDENTE',
+        academia=request.user.academia
+    ).count()
+
     context = {
         'total_alunos': total_alunos,
         'alunos_ativos': alunos_ativos,
@@ -56,6 +123,11 @@ def dashboard(request):
         'aniversariantes': aniversariantes,
         'mensalidades_atrasadas': mensalidades_atrasadas,
         'ranking_frequencia': ranking_frequencia,
+        'faturamento': faturamento,
+        'inadimplentes': inadimplentes,
+        'total_aulas': total_aulas,
+        'meses': meses,
+        'totais': totais,
     }
 
    
