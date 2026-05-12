d6ae23e66bdbf5f26feb5d4115284add4ac32322
Alexandre <aleleonel@gmail.com>
2026-05-12 16:15:09 -0300

    Corrige settings produção render

--- config/settings.py
+++ config/settings.py
@@ -2,36 +2,38 @@ import os
 from pathlib import Path
 import dj_database_url
 
-
-import os
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 ENVIRONMENT = os.environ.get("ENVIRONMENT", "development")
 
+# SECURITY
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY",
+    "dev-secret-key"
+)
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = os.environ.get("SECRET_KEY")
-SECRET_KEY = 'django-insecure-%k+)=n339hzcv)a390baae@i9!2nq18w6op6ej7q@)71ksn%k%'
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = ENVIRONMENT == "development"
 
+# HOSTS
+
 if ENVIRONMENT == "production":
-    ALLOWED_HOSTS = ["dojosystem-v2.onrender.com",]
+    ALLOWED_HOSTS = [
+        "dojosystem-v2.onrender.com",
+        ".onrender.com"
+    ]
+
+    CSRF_TRUSTED_ORIGINS = [
+        "https://*.onrender.com"
+    ]
+
 else:
     ALLOWED_HOSTS = [
-        "localhost",
         "127.0.0.1",
-        ".onrender.com"
-]
+        "localhost"
+    ]
 
-# Application definition
+# APPLICATIONS
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -40,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Meus App
+
     'core',
     'accounts',
     'academias',
@@ -50,9 +52,12 @@ INSTALLED_APPS = [
     'dashboard',
 ]
 
+# MIDDLEWARE
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -61,7 +66,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+# URLS
+
 ROOT_URLCONF = 'config.urls'
+
+# TEMPLATES
 
 TEMPLATES = [
     {
@@ -78,32 +87,20 @@ TEMPLATES = [
     },
 ]
 
+# WSGI
+
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+# DATABASE
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get("DATABASE_URL")
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600
     )
 }
 
-DATABASE_URL = os.environ.get("DATABASE_URL")
-
-if not DATABASE_URL:
-    raise Exception("DATABASE_URL não encontrada")
-
-DATABASES = {
-    'default': dj_database_url.config(
-        default=DATABASE_URL
-    )
-}
-
-
-# Password validation
-# https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
+# PASSWORD VALIDATION
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -120,55 +117,40 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# INTERNATIONALIZATION
 
-# Internationalization
-# https://docs.djangoproject.com/en/6.0/topics/i18n/
+LANGUAGE_CODE = 'pt-br'
 
-LANGUAGE_CODE = "pt-br"
-
-TIME_ZONE = "America/Sao_Paulo"
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
-
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/6.0/howto/static-files/
+# STATIC FILES
 
 STATIC_URL = '/static/'
+
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
-MEDIA_URL = '/media/'
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
+# MEDIA
+
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+# AUTH
 
 AUTH_USER_MODEL = 'accounts.User'
 
 LOGIN_URL = 'login'
-
 LOGIN_REDIRECT_URL = 'dashboard'
-
 LOGOUT_REDIRECT_URL = 'login'
-
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
-
-##SECURE_HSTS_SECONDS = 31536000  # 1 ano
-#SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-#SECURE_HSTS_PRELOAD = True
-#SECURE_SSL_REDIRECT = True
-#SESSION_COOKIE_SECURE = True
-#CSRF_COOKIE_SECURE = True
-
-if ENVIRONMENT == "production":
-    CSRF_TRUSTED_ORIGINS = [
-        "https://*.onrender.com"
-    ]
-
-
-
